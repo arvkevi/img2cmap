60bbb887dd5536383cd55dc28aa5720852863bad
Marshall Krassenstein <marshall.krassenstein@datarobot.com>
2022-05-23 10:53:38 -0400

    Validate web urls on unit tests

--- tests/test_img2cmap.py
+++ tests/test_img2cmap.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import requests
 
 import numpy as np
 import pytest
@@ -7,13 +8,22 @@ from img2cmap import ImageConverter
 
 THIS_DIR = Path(__file__).parent
 
-file_images = list(THIS_DIR.joinpath("images").iterdir())
-web_url_images = [
+test_image_files = list(THIS_DIR.joinpath("images").iterdir())
+image_urls = [
     "https://static1.bigstockphoto.com/3/2/3/large1500/323952496.jpg",
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4N0qxKmiCah2If-5M4Dw7Lb5MPb6w7eNKog&usqp=CAU",
 ]
 
-images = file_images + web_url_images
+# Make sure web urls are valid
+test_image_urls = []
+for url in image_urls:
+    response = requests.get(url)
+    if response.status_code == 200:
+        test_image_urls.append(url)
+    else:
+        print(f"Could not get image from {url}")
+
+images = test_image_files + test_image_urls
 
 
 @pytest.mark.parametrize("test_image_input", images)
@@ -30,7 +40,7 @@ def test_generate_cmap_2(test_image_input):
     assert cmap.N == 4
 
 
-@pytest.mark.parametrize("test_image_input", file_images)
+@pytest.mark.parametrize("test_image_input", test_image_files)
 def test_generate_cmap_3(test_image_input):
     cmap_default_name = test_image_input.stem
     imageconverter = ImageConverter(test_image_input)
