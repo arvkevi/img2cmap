445c4cb5bd5a8d3c3fce2ceeb4663c040ea9c5a4
arvkevi <kevin.arvai@datarobot.com>
2022-10-15 15:44:17 -0400

    bump version

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ project = "img2cmap"
 year = "2022"
 author = "Kevin Arvai"
 copyright = "{0}, {1}".format(year, author)
-version = release = "0.2.2"
+version = release = "0.2.3"
 
 pygments_style = "trac"
 templates_path = ["."]
